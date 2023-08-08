e2b028ef3c9d45b4fa0f09e41dd5c07d0a702188
HirooHG <hugo.golliet@etu.univ-smb.fr>
2023-08-08 19:54:44 +0200

    update(wifi): modified wifi exe

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -1,19 +1,37 @@
 #!/bin/sh
 
 interface="wlan0"
-
 count=0
-path="$HOME/apps-config/scripts/wpa/wpa_supplicant"
+path="$HOME/apps-config/scripts/wpa"
+arg=$1
 
-if [ $1 ]
+function verif {
+  cd $path
+  list=$(ls -a | cut -d'.' -f2)
+  isGood=false
+  for i in $list
+  do
+    if [ $arg == $i ]; then
+      isGood=true
+    fi
+  done
+}
+
+if [ $arg ]
 then
+  verif
+
+  if [ $isGood == false ]; then
+    echo "there's nothing like this" && exit 1
+  fi
+  
   until [ $count == 1 ]
   do
     sudo pkill -e wpa_supplicant
-    count=$(sudo wpa_supplicant -B -i $interface -c $path.$1.conf | wc -l) || echo bruh
+    count=$(sudo wpa_supplicant -B -i $interface -c $path/wpa_supplicant.$arg.conf | wc -l) || echo bruh
     sudo dhcpcd -q
   done
   exit 0
 else
-  exit 1
+  echo "argument pls" && exit 1
 fi
